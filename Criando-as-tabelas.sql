337554a39678bc381b5a0bebb208df782bbe44a9
Luiz <118695802+LuizBrenoDev@users.noreply.github.com>
2024-01-12 09:17:22 -0300

    feat: Corrigindo a tabela produto

--- Criando-as-tabelas.sql
+++ Criando-as-tabelas.sql
@@ -8,7 +8,7 @@ USE shop;
 DROP DATABASE shop;
 
 CREATE TABLE cliente (
-	id_cliente INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
     nome VARCHAR(100) NOT NULL,
     cpf CHAR(11) NOT NULL UNIQUE,
     endereço VARCHAR(150) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE cliente (
 );
 
 CREATE TABLE pedido (
-	id_pedido INT NOT NULL UNIQUE,
+    id_pedido INT NOT NULL UNIQUE,
     id_cliente_do_pedido INT NOT NULL UNIQUE,
     status ENUM('Concluído!', 'Cancelado', 'Em processamento...') NOT NULL,
     entrega_codigo_rastreio INT NOT NULL UNIQUE, 
@@ -27,23 +27,24 @@ CREATE TABLE pedido (
 );
 
 CREATE TABLE produto (
-	id_produto INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    id_produto INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     classificacao ENUM
     ('Livre para todos os públicos', 'Idade Superior ou igual a 14 anos', 'Idade igual ou superior a 16 anos', 'Idade igual ou superior a 18 anos') NOT NULL,
     categoria ENUM ('Eletrônicos', 'Brinquedos', 'Video Game', 'Livros', 'Filmes', 'Utensílios Domésticos', 'Ferramentas', 'Outros') NOT NULL,
+    valor FLOAT NOT NULL,
     avaliacao FLOAT NOT NULL DEFAULT 0.0
 );
 
 CREATE TABLE estoque (
-	id_produto INT UNIQUE NOT NULL,
+    id_produto INT UNIQUE NOT NULL,
     categoria ENUM ('Eletrônicos', 'Brinquedos', 'Video Game', 'Livros', 'Filmes', 'Utensílios Domésticos', 'Ferramentas', 'Outros') NOT NULL,
     quantidade_produto INT NOT NULL DEFAULT 1,
     CONSTRAINT fk_produto_estoque FOREIGN KEY(id_produto) REFERENCES produto(id_produto) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE fornecedor (
-	id_fornecedor INT NOT NULL UNIQUE,
+    id_fornecedor INT NOT NULL UNIQUE,
     razao_social VARCHAR(150) NOT NULL UNIQUE,
     cnpj CHAR(14) NOT NULL,
     contato VARCHAR(100) NOT NULL,
@@ -51,7 +52,7 @@ CREATE TABLE fornecedor (
 );
 
 CREATE TABLE vendedor (
-	id_vendedor INT NOT NULL,
+    id_vendedor INT NOT NULL,
     cnpj CHAR(14) NOT NULL,
     cpf CHAR(11) NOT NULL UNIQUE,
     razao_social VARCHAR(150) NOT NULL UNIQUE,
@@ -61,7 +62,7 @@ CREATE TABLE vendedor (
 );
 
 CREATE TABLE produtos_do_vendedor (
-	id_produto INT NOT NULL UNIQUE,
+    id_produto INT NOT NULL UNIQUE,
     id_vendedor INT NOT NULL AUTO_INCREMENT,
     quantidade_produtos INT NOT NULL,
     PRIMARY KEY (id_produto, id_vendedor),
@@ -71,7 +72,7 @@ CREATE TABLE produtos_do_vendedor (
 );  
 
 CREATE TABLE produtos_do_pedido (
-	id_produto INT NOT NULL UNIQUE,
+    id_produto INT NOT NULL UNIQUE,
     id_pedido INT NOT NULL UNIQUE,
     quantidade_produtos INT NOT NULL,
     disponibilidade ENUM("Disponível", "Indisponível") NOT NULL DEFAULT "Disponível", 
@@ -81,7 +82,7 @@ CREATE TABLE produtos_do_pedido (
 );  
 
 CREATE TABLE produtos_do_fornecedor (
-	id_produto INT NOT NULL UNIQUE,
+    id_produto INT NOT NULL UNIQUE,
     id_fornecedor INT NOT NULL,
     quantidade_produtos INT NOT NULL,
     disponibilidade ENUM("Disponível", "Indisponível") NOT NULL DEFAULT "Indisponível", 
@@ -91,6 +92,6 @@ CREATE TABLE produtos_do_fornecedor (
 );  
 
 CREATE TABLE entrega (
-	status ENUM("A caminho...", "Partindo para entrega", "Chegou") NOT NULL DEFAULT "Partindo para entrega",
+    status ENUM("A caminho...", "Partindo para entrega", "Chegou") NOT NULL DEFAULT "Partindo para entrega",
     codigo_rastreio INT NOT NULL UNIQUE PRIMARY KEY
 );
